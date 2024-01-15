7cafda1e65600b78a42740cc899bf35a0a00c4e2
Nico Mühling <nico.muehling95@gmial.com>
2024-01-15 18:17:37 +0100

    main program loop here
    added CropAndNormalize for converting raw image byte to tensor

--- Program.cs
+++ Program.cs
@@ -1,37 +1,79 @@
-﻿using System.Data.SqlTypes;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Drawing.Imaging;
+using System.Numerics;
+using System.Text;
+using Newtonsoft.Json;
+using PokeTorchAi.Ai;
+using PokeTorchAi.Pokemon.Classes;
+using PokeTorchAi.PyBoyInteract;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using Tensorboard;
 using TorchSharp;
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using TorchSharp;
-using static TorchSharp.torch.nn;
-using static TorchSharp.torch;
-using static TorchSharp.TensorExtensionMethods;
-using static TorchSharp.torch.distributions;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using Rectangle = System.Drawing.Rectangle;
+
+
 namespace PokeTorchAi;
+
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        
-        Random rng = new Random();
-        var cnt = 0;
-        var agent = new Agent(numActions: 10, batchSize: 16, discountFactor: 0.99f, epsilon: 1.0f, epsilonDecay: 0.995f, minEpsilon: 0.01f);
-        agent.LoadModel();
-            for (int i = 0; i < 10_000; i++)
-            {
-                agent.UpdateExperienceMemory(  torch.rand(1,144,144), rng.Next(0,10), (float)rng.NextDouble(), torch.rand(1,144,144));
-            }
-        while (cnt != 500)
-        {
+    private const int NumActions = 8;
 
-            Console.WriteLine(agent.SelectAction(torch.rand( 1, 144, 144)));
-            
+    private static void Main(string[] args)
+    {
+        var rng = new Random();
+        var agent = new Agent(NumActions, batchSize: 16, discountFactor: 0.99f, epsilon: 1.0f, epsilonDecay: 0.995f,
+            minEpsilon: 0.01f);
+        agent.LoadModel();
+
+
+        var memoryValues = new List<MemoryValue>();
+        var pipeServer = new PipeServer();
+        torch.Tensor? imageCurrent = null;
+        var firstRun = true;
+        pipeServer.StartServer();
+        while (true)
+        {
+            var imageBefore = imageCurrent;
+            var byteImageCurrent = GetCurrentImageAsBytes(pipeServer);
+            imageCurrent = CropAndNormalize(byteImageCurrent);
+            memoryValues = GetCurrentMemoryValues(pipeServer);
+            var lastMovement = agent.SelectAction(imageCurrent);
+            pipeServer.SendMovementData(lastMovement);
+
+            if (firstRun)
+            {
+                firstRun = false;
+                continue;
+            }
+            agent.UpdateExperienceMemory(imageBefore!,(int)lastMovement,0.0f,imageCurrent);
             agent.UpdateModel();
-            cnt++;
         }
-        agent.SaveModel();
     }
     
+    
+    private static torch.Tensor CropAndNormalize(byte[] stateImage)
+    {
+        var transform = 
+            torchvision.transforms.Compose([
+            torchvision.transforms.CenterCrop(144, 144),
+            torchvision.transforms.Grayscale()
+        ]);
+        return transform.call(torch.tensor(stateImage,torch.ScalarType.Float32).view(3,160,144));
+    }
+
+
+    private static List<MemoryValue> GetCurrentMemoryValues(PipeServer pipeServer)
+    {
+        var byteMemVals = pipeServer.GetData();
+        return JsonConvert.DeserializeObject<List<MemoryValue>>(Encoding.UTF8.GetString(byteMemVals))!;
+    }
+
+    private static byte[] GetCurrentImageAsBytes(PipeServer pipeServer)
+    {
+        return pipeServer.GetData();
+    }
 }
