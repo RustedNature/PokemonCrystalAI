5d46b8cda7b20fd361bc265cd70e7001366bd697
Nico Mühling <nico.muehling95@gmial.com>
2024-01-15 18:13:00 +0100

    Added a class for easy access to memory values
    in python to with to_dict for json serializing

---
+++ Pokemon/Classes/MemoryValue.cs
@@ -0,0 +1,16 @@
+﻿using PokeTorchAi.Pokemon.Enums;
+
+namespace PokeTorchAi.Pokemon.Classes
+{
+    public class MemoryValue
+    {
+        public MemoryAddresses MemAddress { get; set; }
+        public int MemValue { get; set; }
+
+        public MemoryValue(int memAddress, int memValue)
+        {
+            MemAddress = (MemoryAddresses)memAddress;
+            MemValue = memValue;
+        }
+    }
+}
