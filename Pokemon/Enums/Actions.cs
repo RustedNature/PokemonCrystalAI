73be86564e2b5df3f3331e4764c296eb512309b3
Nico Mühling <nico.muehling95@gmial.com>
2024-01-15 18:04:26 +0100

    Add Actions enum in Pokemon project
    Enum for int representation of button presses

---
+++ Pokemon/Enums/Actions.cs
@@ -0,0 +1,13 @@
+﻿namespace PokeTorchAi.Pokemon.Enums;
+
+public enum Actions
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3,
+    A = 4,
+    B = 5,
+    Start = 6,
+    Select = 7,
+}
