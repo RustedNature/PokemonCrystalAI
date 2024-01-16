aa10884a3f5c54fddaf688f0322819928a5f2ea9
Nico Mühling <nico.muehling95@gmial.com>
2024-01-16 08:13:06 +0100

    Added handling for targetModel

--- Ai/Agent.cs
+++ Ai/Agent.cs
@@ -15,13 +15,13 @@ public class Agent(
     float epsilonDecay = 0.995f,
     float minEpsilon = 0.01f)
 {
-    
+    private int _updateCounter = 0;
     private const string ModelPath = "MODEL";
     private const string ModelName = "PokeNet.pt";
     private static readonly Random Rng = new Random();
-    private readonly ExperienceReplay _replayMemory = new ExperienceReplay(10_000);
-    private readonly PokeAi _model = new PokeAi("PokeNetModel", numActions, 1).cuda();
-    private readonly PokeAi _targetModel = new PokeAi("PokeNetTarget", numActions, 1).cuda();
+    private readonly ExperienceReplay _replayMemory = new ExperienceReplay(100_000);
+    private PokeAi _model = new PokeAi("PokeNetModel", numActions, 1).cuda();
+    private PokeAi _targetModel = new PokeAi("PokeNetTarget", numActions, 1).cuda();
 
 
     public long SelectAction(torch.Tensor stateImage)
@@ -35,10 +35,12 @@ public class Agent(
             withNoGrad.Dispose();
             _model.train();
             action = qValues.argmax().item<long>();
+            Console.WriteLine("PREDICT");
         }
         else
         {
             action = Rng.Next(0, numActions);
+            Console.WriteLine("RANDOM");
         }
 
         return action;
@@ -48,6 +50,10 @@ public class Agent(
 
     public void UpdateModel()
     {
+        if (_replayMemory.GetMemoryCount() < batchSize)
+        {
+            return;
+        }
         using var optimizer = torch.optim.Adam(_model.parameters());
         optimizer.zero_grad();
         var experiences = _replayMemory.Sample(batchSize);
@@ -64,6 +70,25 @@ public class Agent(
 
         optimizer.step();
         epsilon = Math.Max(minEpsilon, epsilon * epsilonDecay);
+        Console.WriteLine(epsilon);
+        ++_updateCounter;
+        UpdateTargetModel();
+    }
+
+    private void UpdateTargetModel()
+    {
+        if ((_updateCounter + 1) % 10_000 != 0) return;
+        var cpuModel = _model.cpu();
+        var cpuTargetModel = _targetModel.cpu();
+
+        cpuTargetModel.load_state_dict(cpuModel.state_dict());
+
+        Console.WriteLine("Model parameters copied.");
+
+        _model = cpuModel.cuda();
+        _targetModel = cpuTargetModel.cuda();
+
+        Console.WriteLine("Models moved back to GPU.");
     }
 
     public void UpdateExperienceMemory(torch.Tensor state, int action, float reward, torch.Tensor nextState)
@@ -75,19 +100,25 @@ public class Agent(
     {
         using var fs = new FileStream(ModelPath + "/" + ModelName, FileMode.Create);
         _model.save(fs);
+        using var fs2 = new FileStream(ModelPath + "/" + ModelName + "Target", FileMode.Create);
+        _targetModel.save(fs2);
+
     }
 
-    public void LoadModel()
+    public bool LoadModel()
     {
         if (!Directory.Exists(ModelPath + "/"))
         {
             Directory.CreateDirectory(ModelPath + "/");
-            return;
+            return false;
         }
 
-        if (!File.Exists(ModelPath + "/" + ModelName)) return;
+        if (!File.Exists(ModelPath + "/" + ModelName)) return false;
 
         using var fs = new FileStream(ModelPath + "/" + ModelName, FileMode.Open);
         _model.load(fs);
+        using var fs2 = new FileStream(ModelPath + "/" + ModelName + "Target", FileMode.Open);
+        _targetModel.load(fs2);
+        return true;
     }
 }
